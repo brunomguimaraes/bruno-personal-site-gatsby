4a3bef7bc8b431ab271eb9d2574c57cabefb9583
Bruno Guimarães <39094505+brunomguimaraes@users.noreply.github.com>
2020-04-26 17:57:27 -0300

    adds cubos description (#8)

--- src/global/constants.js
+++ src/global/constants.js
@@ -2,7 +2,7 @@ export const JobExperiences = [
   {
     title: "Jusbrasil",
     role: "Software Engineer",
-    date: "Since 2019",
+    date: "Since November, 2019",
     location: "Salvador-BA, Brazil",
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -18,12 +18,13 @@ export const JobExperiences = [
     role: "Frontend Developer",
     date: "January, 2019 - November, 2019",
     location: "Salvador-BA, Brazil",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    description: `After falling in love with learning programming and deciding to pivot my carrer to Software Develoment,
+      Cubos gave me that chance and pushed me to my limits, fortunately, I was able to work alongside brilliant colleagues
+      and those times where crucial for my carrer.
+      I've had the opportunity to work on several projects that laid the foundation for my software engineering
+      career.
+      I'm grateful for the time I spent with these folks, whose work gave me a glimpse into
+      the challenges and rewards of a developer's life.`,
     photo: "cubos",
     url: "https://cubos.io/",
   },
@@ -32,12 +33,9 @@ export const JobExperiences = [
     role: "Engineering Intern",
     date: "May, 2016 - May, 2018",
     location: "Camaçari-BA, Brazil",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    description: `This was my first internship, I had the opportunity to work as a team to solve
+      problems and come up with solutions to increase overall plant productivity and efficiency.
+      At White Martins was where I learned how to articulate my ideas and transform those into results.`,
     photo: "white",
     url: "http://www.praxair.com.br/",
   },
