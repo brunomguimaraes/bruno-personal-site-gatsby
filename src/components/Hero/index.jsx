b3d0800c19ce87b10b45f8bb8ab1427b38db7653
Bruno <mguimaraesbruno@gmail.com>
2020-07-06 00:15:14 -0300

    first color studies

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,9 +5,8 @@ import styles from "./styles.module.scss"
 
 const Hero = () => (
   <div className={styles["hero"]}>
-    <h1 className={styles["heroHeader"]}>Hi, my name is Bruno</h1>
+    <h1 className={styles["heroHeader"]}>hi, my name is <strong>Bruno Guimarães</strong></h1>
     <h2 className={styles["heroSubHeader"]}>and I'm a software engineer</h2>
-    <RocketSVG className={styles["heroRocket"]} />
   </div>
 )
 
