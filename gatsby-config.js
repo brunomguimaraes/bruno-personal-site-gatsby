43a403f80493444b37b3b51319a963432e04f615
Bruno Guimarães <39094505+brunomguimaraes@users.noreply.github.com>
2020-07-21 14:18:16 -0300

    V2 (#11)
    * first color studies
    
    * adds hero style and svg
    
    * base styles changed
    
    * v2 new layout
    
    * update metadata
    
    * removes comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,9 @@ module.exports = {
   siteMetadata: {
     title: `Bruno Guimarães`,
     description: `Personal website from someone who is passionate for technology and love to come up with smart solutions to our everyday problems.`,
+    url: "https://www.brunomoraesguimaraes.com.br",
+    image: "/images/bgny.jpg", 
+    twitterUsername: "@brunaodev",
     author: `@brunaodev`,
   },
   plugins: [
