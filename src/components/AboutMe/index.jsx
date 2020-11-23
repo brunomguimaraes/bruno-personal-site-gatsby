4f53c19bc8fd93e2884766dd14d8d905692c00b3
Bruno Guimarães <39094505+brunomguimaraes@users.noreply.github.com>
2020-11-23 09:06:18 -0300

    Fix major typo

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -11,7 +11,7 @@ const AboutMe = () => (
     </div>
     <SectionTitle text={`About Me`} />
     <span className={styles["aboutMeDescription"]}>
-      I'm a Software Enginner based in Salvador, Brazil. I have a
+      I'm a Software Engineer based in Salvador, Brazil. I have a
       passion for technology and love to come up with smart solutions to our
       daily problems.
     </span>
