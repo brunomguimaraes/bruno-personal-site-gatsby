01c7bf473512aa4204ff090595e4d51db7248e35
Bruno Guimarães <mguimaraesbruno@gmail.com>
2023-05-31 08:54:50 -0300

    v2 kickoff

--- src/components/SectionTitle/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import styles from "./styles.module.scss"
-
-const SectionTitle = ({ text }) => (
-  <h3 className={styles["headerText"]}>{`━━━━  ${text}  ━━━━`}</h3>
-)
-
-export default SectionTitle
